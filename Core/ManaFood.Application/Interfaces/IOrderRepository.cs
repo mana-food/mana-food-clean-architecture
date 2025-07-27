a977d944fed58bef1e7c51ecaecd6c3a92b7471f
Vitória Vitoria <vitoriasilva98@hotmail.com>
2025-07-27 17:05:09 -0300

    feat: Ordenação e filtragem dos pedidos (Pronto > Em preparação > Recebido)

--- Core/ManaFood.Application/Interfaces/IOrderRepository.cs
+++ Core/ManaFood.Application/Interfaces/IOrderRepository.cs
@@ -1,7 +1,9 @@
-﻿using ManaFood.Domain.Entities;
+﻿using System.Linq.Expressions;
+using ManaFood.Domain.Entities;
 
 namespace ManaFood.Application.Interfaces;
 
 public interface IOrderRepository : IBaseRepository<Order>
 {
+    Task<List<Order>> GetAllActive(Expression<Func<Order, bool>> predicate, CancellationToken cancellationToken);
 }
--- Infrastructure/ManaFood.Infrastructure/Database/Repositories/OrderRepository.cs
+++ Infrastructure/ManaFood.Infrastructure/Database/Repositories/OrderRepository.cs
@@ -1,10 +1,20 @@
-﻿using ManaFood.Domain.Entities;
+﻿using System.Linq.Expressions;
+using ManaFood.Domain.Entities;
 using ManaFood.Application.Interfaces;
 using ManaFood.Infrastructure.Database.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace ManaFood.Infrastructure.Database.Repositories;
 
 public class OrderRepository : BaseRepository<Order>, IOrderRepository
 {
     public OrderRepository(ApplicationContext applicationContext) : base(applicationContext) { }
+    
+    public async Task<List<Order>> GetAllActive(Expression<Func<Order, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return await _applicationContext.Set<Order>()
+            .Where(x => !x.Deleted)
+            .Where(predicate)
+            .ToListAsync(cancellationToken);
+    }
 }
