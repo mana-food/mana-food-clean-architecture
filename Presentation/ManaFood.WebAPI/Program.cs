c99700f3faec2b7e700b95a44b20106fe80a0051
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2025-09-24 20:14:25 -0300

    fix: corrigindo vazamento de dominio

--- Presentation/ManaFood.WebAPI/Program.cs
+++ Presentation/ManaFood.WebAPI/Program.cs
@@ -7,27 +7,11 @@ using ManaFood.Application.Interfaces;
 using ManaFood.Application.Interfaces.Services;
 using ManaFood.Application.Services;
 
-using ManaFood.Infrastructure.Auth;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-var jwtSettings = builder.Configuration.GetSection("Jwt");
 
-// JWT & Auth services
-builder.Services.AddSingleton<ITokenBlacklistService, TokenBlacklistService>();
-builder.Services.AddSingleton<IJwtService>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    var jwtSettings = configuration.GetSection("Jwt");
-    var secretKey = jwtSettings["SecretKey"];
-    var expiration = int.Parse(jwtSettings["ExpirationMinutes"]);
-    var issuer = jwtSettings["Issuer"];
-    var audience = jwtSettings["Audience"];
-    var blacklistService = provider.GetRequiredService<ITokenBlacklistService>();
-    return new JwtService(secretKey, expiration, issuer, audience, blacklistService);
-});
+builder.Services.ConfigureAuth(builder.Configuration);
 
 // Configurações de infraestrutura e aplicação
 builder.Services.ConfigurePersistenceApp();
@@ -76,27 +60,6 @@ builder.Services.AddHttpClient<IPaymentStatusService, MercadoPagoStatusService>(
 builder.Services.AddScoped<IPaymentProviderConfig, PaymentProviderConfig>();
 builder.Services.AddScoped<IAuthAppService, AuthAppService>();
 
-// Autenticação JWT
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = "JwtBearer";
-    options.DefaultChallengeScheme = "JwtBearer";
-})
-.AddJwtBearer("JwtBearer", options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings["Issuer"],
-        ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]))
-    };
-});
-
 var app = builder.Build();
 
 // Aplica migrations automaticamente
@@ -112,10 +75,11 @@ app.UseSwaggerUI(c =>
 
 app.UseHttpsRedirection();
 
+// app.UseMiddleware<ManaFood.WebAPI.Middlewares.JwtAuthenticationMiddleware>();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseMiddleware<ManaFood.WebAPI.Middlewares.JwtAuthenticationMiddleware>();
 
 app.MapControllers();
 app.Run();
